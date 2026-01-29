fd842ebd5532123fbe89f68224ec7ea51cbd524f
mirdayan <dmirandaherrera@gmail.com>
2026-01-28 21:58:10 -0500

    se agrega evento,usando el DOM,  al aplicar click sobre el boton aplicar, cambia al estado aplicado.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-const boton = document.querySelector('#boton-importante')
-boton.addEventListener('click', () => {
+const botonSeccion = document.querySelector('.jobs-listings')
+botonSeccion.addEventListener('click', (e) => {
+    const boton = e.target
+
+
+    if (boton.classList.contains('button-apply-job')) {
+
     boton.textContent = '¡Aplicado!'
     boton.classList.add('is-applied')
     boton.disabled = true
+
+    }
+
 })
